3f6cea8ca40c4372e4b2bbb57dd9ed2122e5c5fb
Ramiro Busto <ramirobus81@gmail.com>
2023-09-13 12:22:41 -0300

    preEntrega1 Terminada

--- main.js
+++ main.js
@@ -7,15 +7,16 @@ let cantidad = 0;
 
 function materiaIngresada(){
     if(NOMBRE_COMPLETO !== null && NOMBRE_COMPLETO !== NaN) {
-        let materia = prompt("Hola " + NOMBRE_COMPLETO + ", ¿En cual Materia queres consultar tu promedio?")
-        alert("Buenisimo!" + " Para saber tu promedio en " + materia + ", ingrese sus calificaciones a continuación...")
+        let materia = prompt("Hola " + NOMBRE_COMPLETO + ", ¿En cual Materia queres consultar tu promedio?");
+        alert("Buenisimo!" + " Para saber tu promedio en " + materia + ", ingrese sus calificaciones a continuación...");
+        return materia;
     }
 }
 
 
 materiaIngresada();
 let calificacion = parseFloat(prompt("Ingrese la nota N°1:"));
-while (calificacion !== NaN && calificacion > 0 && calificacion < 10.01){
+while (!isNaN(calificacion) && calificacion > 0 && calificacion < 10.01){
     total = total + calificacion;
     cantidad++;
     calificacion = parseFloat(prompt("Ingrese la nota N°" + (cantidad + 1) + ":"));
@@ -23,12 +24,12 @@ while (calificacion !== NaN && calificacion > 0 && calificacion < 10.01){
 
 
 promedio = total / cantidad;
-function notaFinal(){
+function notaFinal() {
     if (promedio >= 7) {
-        alert("Felicitaciones! Aprobaste" + materia + nombre + "! " + "Tu promedio es de " + promedio);
+        alert("Felicitaciones! Aprobaste " + nombre + " !Tu promedio es de " + promedio);
     } else if (promedio < 7) {
-        alert(nombre + ", lamentamos informarte que Reprobaste " + materia + "Tu promedio es de " + promedio);
+        alert(nombre + ", lamentamos informarte que reprobaste " + ". Tu promedio es de " + promedio);
     }
 }
-alert(promedio)
+
 notaFinal();
