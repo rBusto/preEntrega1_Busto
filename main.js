ef0540efe4326ef20fb9bc0720152e67dc3b934b
Ramiro Busto <ramirobus81@gmail.com>
2023-09-13 15:19:10 -0300

    contrato

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const MAYUSCULA = (string) => {
+    /*
+    Propósito: Cada String ingresado por el usuario, sin importar como se ingrese,
+    la letra inicial se pasara a Mayuscula y las demas a Minuscula.
+    */
     return string[0].toUpperCase() + string.substring(1).toLowerCase();
 }
 
@@ -12,6 +16,10 @@ let promedio = 0;
 let cantidad = 0;
 
 function materiaIngresada(){
+    /*
+    Propósito: Pregunta al Usuario una Materia y sus calificaciones, pasa el string a Mayuscula y retorna el valor
+    de la variable materia.
+    */
     if(NOMBRE_COMPLETO !== null && NOMBRE_COMPLETO !== NaN) {
         let materia = prompt("Hola " + NOMBRE_COMPLETO + ", ¿En cual Materia queres consultar tu promedio?");
         materia = MAYUSCULA(materia);
@@ -32,6 +40,11 @@ while (!isNaN(calificacion) && calificacion > 0 && calificacion < 10.01){
 
 promedio = total / cantidad;
 function notaFinal() {
+    /*
+    Propósito: Si el promedio de la suma total de las notas ingresadas por el usuario es mayor o igual a 7,
+    se muestra un mensaje de Aprobado con su respectivo Promedio, en caso contrario, se muestra un mensaje 
+    diciendo que Desaprobo, con su respectivo Promedio.
+    */
     if (promedio >= 7) {
         alert("Felicitaciones! Aprobaste " + nombre + " !Tu promedio es de " + promedio);
     } else if (promedio < 7) {
